3e3706889df1f46c9a4a68aac01315c63f2e595c
Vibin <cybervibz@gmail.com>
2025-12-25 19:47:18 +0530

    Major cleanup: Move debug scripts, split build.py into modules, update gitignore, output to dist/

--- clear_refreshed.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('C:/Users/vibin/AppData/Roaming/Shakshuka/data/shakshuka.db')
-conn.execute('UPDATE tasks SET refreshed_at = NULL WHERE refreshed_at LIKE "2025-12-12:%"')
-conn.commit()
-print('Cleared today\'s refreshed_at timestamps')
-conn.close()
